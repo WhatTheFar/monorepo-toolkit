e2a41a36b97a3ca6354b476656276124eafd987f
WhatTheFar <jakpat.m@gmail.com>
2020-06-30 02:32:34 +0700

    feat(uc): ListChangesUseCase

---
+++ pkg/usecase/list-changes.go
@@ -0,0 +1,46 @@
+//go:generate mockgen -destination mock/list-changes.go . ListChangesUseCase
+
+package usecase
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/whatthefar/monorepo-toolkit/pkg/core"
+)
+
+type ListChangesUseCase interface {
+	ListChanges(ctx context.Context, paths []string, workflowID string) ([]string, error)
+}
+
+type listChangesUseCase struct {
+	git      core.GitGateway
+	pipeline core.PipelineGateway
+}
+
+func (uc *listChangesUseCase) ListChanges(ctx context.Context, paths []string, workflowID string) ([]string, error) {
+	lastCommit, err := uc.pipeline.LastSuccessfulCommit(ctx, workflowID)
+	if err != nil {
+		return nil, errors.Wrapf(err, "can't get last succesful commit for workflow ID %s", workflowID)
+	}
+	currentCommit := uc.pipeline.CurrentCommit()
+
+	changes, err := uc.git.DiffNameOnly(core.Hash(lastCommit), core.Hash(currentCommit))
+
+	return filterOnlyPathsWithChanges(paths, changes), nil
+}
+
+func filterOnlyPathsWithChanges(paths []string, changes []string) []string {
+	changesJoined := strings.Join(changes, "\n")
+	pathsWithChanges := make([]string, 0)
+	for _, path := range paths {
+		re := regexp.MustCompile(fmt.Sprintf(`(?m)^%s`, path))
+		if re.MatchString(changesJoined) == true {
+			pathsWithChanges = append(pathsWithChanges, path)
+		}
+	}
+	return pathsWithChanges
+}
---
+++ pkg/usecase/list-changes_test.go
@@ -0,0 +1,117 @@
+package usecase
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/whatthefar/monorepo-toolkit/pkg/core"
+	mock_core "github.com/whatthefar/monorepo-toolkit/pkg/core/mock"
+)
+
+const (
+	gitFixtureBasicPath = "../../test/git-fixtures/basic"
+)
+
+func TestListChangesUseCase(t *testing.T) {
+	Convey("Given a listChangesUseCase", t, func() {
+		ctx := context.Background()
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		git := mock_core.NewMockGitGateway(ctrl)
+		pipeline := mock_core.NewMockPipelineGateway(ctrl)
+
+		uc := &listChangesUseCase{git, pipeline}
+
+		Convey("When calls Execute", func() {
+			paths := []string{"services/app1"}
+			workflowID := "main.yml"
+
+			lastCommit := core.Hash("123")
+			currentCommit := core.Hash("456")
+
+			ctxType := reflect.TypeOf((*context.Context)(nil)).Elem()
+			pipeline.EXPECT().
+				LastSuccessfulCommit(gomock.AssignableToTypeOf(ctxType), gomock.Eq(workflowID)).
+				Return(lastCommit, nil)
+			pipeline.EXPECT().
+				CurrentCommit().
+				Return(currentCommit)
+			git.EXPECT().
+				DiffNameOnly(
+					gomock.Eq(lastCommit),
+					gomock.Eq(currentCommit),
+				).
+				Return([]string{
+					"services/app1/README.md",
+					"services/app2/README.md",
+					"services/app3/README.md",
+				}, nil)
+
+			want := []string{
+				"services/app1",
+			}
+
+			got, err := uc.ListChanges(ctx, paths, workflowID)
+
+			So(err, ShouldBeNil)
+			So(got, ShouldResemble, want)
+		})
+	})
+}
+
+func TestFilterOnlyPathsWithChanges(t *testing.T) {
+	cases := []*struct {
+		paths    []string
+		changes  []string
+		expected []string
+	}{
+		{
+			paths: []string{"services/app1", "services/app2"},
+			changes: []string{
+				"services/app1/README.md",
+				"services/app2/README.md",
+				"services/app3/README.md",
+			},
+			expected: []string{"services/app1", "services/app2"},
+		},
+		{
+			paths: []string{"services/app1/README.md", "services/app2/README.md"},
+			changes: []string{
+				"services/app1/README.md",
+				"services/app2/README.md",
+				"services/app3/README.md",
+			},
+			expected: []string{"services/app1/README.md", "services/app2/README.md"},
+		},
+		{
+			paths: []string{"pkg", "services/app3"},
+			changes: []string{
+				"services/app1/README.md",
+				"services/app2/README.md",
+				"services/app3/README.md",
+			},
+			expected: []string{"services/app3"},
+		},
+	}
+
+	for i, v := range cases {
+		var (
+			paths   = v.paths
+			changes = v.changes
+			want    = v.expected
+		)
+		t.Run(fmt.Sprintf("Case %d, filterOnlyPathsWithChanges should work", i), func(t *testing.T) {
+			var got []string
+			got = filterOnlyPathsWithChanges(paths, changes)
+
+			assert.Equal(t, want, got)
+		})
+	}
+}
